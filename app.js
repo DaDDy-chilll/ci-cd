958750202c0ef1a330adcc4270c69c4e8d195ea6
DaDDy <blacklistuser420@gmail.com>
2023-04-27 18:54:02 +0630

    add test

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8000;
 app.use(cors({ origin: "*" }));
-
+app.use(express.json());
 app.get("/names", (req, res) => {
   res.status(200).json({
     name: "DaDDy Chill",
   });
 });
 
-async function startServer() {
-  app.listen(PORT, () => {
-    console.log(`Server is running on Port:${PORT}`);
+app.post("/names", (req, res) => {
+  if (!req.body) {
+    throw console.error();
+  }
+
+  res.status(201).json({
+    success: "You are create successful",
   });
-}
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Port:${PORT}`);
+});
+
 module.exports = app;
--- app.test.js
+++ app.test.js
@@ -11,3 +11,17 @@ describe("Test Get /names", () => {
     expect(data.body.name).toBe("DaDDy Chill");
   });
 });
+
+describe("Test POST /names", () => {
+  test("It Should respond  201 match success message", async () => {
+    const data = { name: "daddy" };
+    const response = await request
+      .post("/names")
+      .send(data)
+      .expect(201)
+      .expect("Content-Type", /json/);
+    expect(response.body).toStrictEqual({
+      success: "You are create successful",
+    });
+  });
+});
